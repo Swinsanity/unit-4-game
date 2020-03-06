17bae320a4bbfb04c30780266bd2f0d863f0141a
Swinsanity <jesseswinsanity@gmail.com>
2020-03-05 21:51:31 -0500

    Added global variables, created randomNumber function, started crystal buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,40 @@ console.log("Yo!")
 
 $(document).ready(function() {
 
-    
+    var crystal = $(".crystal");
+    var ruby = $("#ruby");
+    var sapphire = $("#sapphire");
+    var gold = $("#gold");
+    var emerald = $("#emerald");
+    var randomCrystal = Math.floor((Math.random() * 12) + 1);
+    var rubyNumber = randomCrystal
+    var saphNumber = randomCrystal
+    var goldNumber = randomCrystal
+    var greenNumber = randomCrystal
 
+    $("#randomNumber").html(function() {
+
+        var minNumber = 19;
+
+        var maxNumber = 120;
+
+        var randomNumber = Math.floor((Math.random() * maxNumber) + minNumber);
+
+        $("#randomNumber").text(randomNumber)
+    });
+
+    $("#currentNumber").html(function() {
+
+       var currentNumber = (rubyNumber + saphNumber + goldNumber + greenNumber)
+
+       console.log(currentNumber)
+
+    });
+
+    $("#ruby").on("click", function() {
+
+
+        
+    });
 
 });
