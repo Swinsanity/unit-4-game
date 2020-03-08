f33b137b79676f5287f105c8f9f9d26f2bf8aa4c
Swinsanity <jesseswinsanity@gmail.com>
2020-03-08 09:19:24 -0400

    Changed variables to streamline buttons. Now working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,35 +10,53 @@ $(document).ready(function() {
     var sapphire = $("#sapphire");
     var gold = $("#gold");
     var emerald = $("#emerald");
-    var randomCrystal = Math.floor((Math.random() * 12) + 1);
-    var rubyNumber = randomCrystal
-    var saphNumber = randomCrystal
-    var goldNumber = randomCrystal
-    var greenNumber = randomCrystal
+    var randomRuby = Math.floor((Math.random() * 12) + 1);
+    var randomSapphire = Math.floor((Math.random() * 12) + 1);
+    var randomGold = Math.floor((Math.random() * 12) + 1);
+    var randomEmerald = Math.floor((Math.random() * 12) + 1);
+    var currentNumber = 0
 
     $("#randomNumber").html(function() {
 
-        var minNumber = 19;
 
-        var maxNumber = 120;
-
-        var randomNumber = Math.floor((Math.random() * maxNumber) + minNumber);
+        var randomNumber = 19 + Math.floor(Math.random() * 120);
 
         $("#randomNumber").text(randomNumber)
     });
 
-    $("#currentNumber").html(function() {
-
-       var currentNumber = (rubyNumber + saphNumber + goldNumber + greenNumber)
-
-       console.log(currentNumber)
-
-    });
 
     $("#ruby").on("click", function() {
-
-
+        
+        currentNumber += randomRuby
+        console.log(currentNumber)
+        $("#currentNumber").text(currentNumber)
         
     });
+
+    $("#sapphire").on("click", function() {
+        
+        currentNumber += randomSapphire
+        console.log(currentNumber)
+        $("#currentNumber").text(currentNumber)
+        
+    });
+
+    $("#gold").on("click", function() {
+        
+        currentNumber += randomGold
+        console.log(currentNumber)
+        $("#currentNumber").text(currentNumber)
+        
+    });
+
+    $("#emerald").on("click", function() {
+        
+        currentNumber += randomEmerald
+        console.log(currentNumber)
+        $("#currentNumber").text(currentNumber)
+        
+    });
+
+
 
 });
